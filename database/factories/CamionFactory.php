c2fa0b8635fc5d55a1fa7df7632db6a07daaefcc
Helen <lissa.acevedo99@gmail.com>
2024-05-13 21:27:28 -0600

    Se creo seeders y factories de:Camiones, Transportistas y Ingreso. De igualmanera se crearon los modelos de estos.

---
+++ database/factories/CamionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+use App\Models\Camion;
+use App\Models\Transportista;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CamionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    protected $camion=Camion::class;
+    public function definition()
+    {
+        return [
+            'placa'=> $this->faker->unique()->numerify(),
+            'marca' =>$this->faker->name,
+            'modelo' =>$this->faker->paragraph,
+            'capacidad'=>$this->faker->numerify(),
+            'id_transportista' => Transportista::inRandomOrder()->first()->id
+        ];
+    }
+}
